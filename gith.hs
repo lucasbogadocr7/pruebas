34c8ba75efca76526abc3fea7bd3a592a5d87728
lucasbogadocr7 <lubogado@frba.utn.edu.ar>
2024-05-02 22:08:41 -0300

    segundo

--- gith.hs
+++ gith.hs
@@ -1,3 +1,10 @@
 --gith
 esMulDe :: Integral a => a -> a -> Bool
 esMulDe num1 num2 = num1 `mod` num2 == 0
+
+--9
+promedios :: Fractional a =>[[a]] -> [a]
+promedios listagorda = map promedio listagorda
+
+promedio :: Fractional a => [a] -> a
+promedio lista = (sum lista) / fromIntegral (length lista)
